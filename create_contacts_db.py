050f6cc967db0ae9ffb0488fd318cb2c155a6f08
wprudencio97 <36481731+wprudencio97@users.noreply.github.com>
2020-01-26 12:14:33 -0500

    updated create_contacts_db.py

--- create_contacts_db.py
+++ create_contacts_db.py
@@ -1,20 +1,21 @@
 import sqlite3
 
+
 def main():
-    #open the txt file
+    # open the txt file
     file = open("contactsData.txt", "r")
 
-    #define the data list
+    # define the data list
     dataList = []
 
-    #read the lines from the txt file
+    # read the lines from the txt file
     for line in file:
         line = line.rstrip("\n")
         dataList.append(line)
 
     # Create a connection.
     conn = sqlite3.connect('contacts.db')
-    
+
     # Create a cursor
     c = conn.cursor()
     c.execute("DROP TABLE CONTACT")
@@ -23,24 +24,25 @@ def main():
               "   CONTACT_ID INTEGER PRIMARY KEY," +
               "   NAME TEXT, " +
               "   EMAIL TEXT)")
-    
+
     for i in dataList:
-        #split i into name and email
+        # split i into name and email
         name = i.split(",")[0]
         email = i.split(", ")[1]
 
-        #insert the values into the database
-        values=(name, email)
-        sql="INSERT INTO CONTACT (NAME,EMAIL) VALUES (?,?)"
-        c.execute(sql,values)
-    
+        # insert the values into the database
+        values = (name.strip("'"), email.strip("'"))
+        sql = "INSERT INTO CONTACT (NAME,EMAIL) VALUES (?,?)"
+        c.execute(sql, values)
+
     # Save (commit) the changes
     conn.commit()
-    
+
     # Close the connection.
     conn.close()
 
-    #close the text file
+    # close the text file
     file.close()
+
 
 main()
