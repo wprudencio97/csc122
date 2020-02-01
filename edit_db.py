9fc9ff10f8352012dbcfb967cd058ff483a96cec
wprudencio97 <wprudencio97@gmail.com>
2020-02-01 08:36:56 -0500

    Added comments to edit_db.py

--- edit_db.py
+++ edit_db.py
@@ -1,11 +1,7 @@
 import sqlite3
 
 # define the main function
-
-
 def main():
-    print("Hello")
-
     # Create a connection.
     conn = sqlite3.connect('contacts.db')
 
@@ -13,7 +9,7 @@ def main():
     c = conn.cursor()
 
     # get the user action and call the appropriate function
-    userAction = input("Enter (C)reate, (U)pdate, or (D)elete: ")
+    userAction = input("\nEnter (C)reate, (U)pdate, or (D)elete: ")
 
     if(userAction.lower() == "c" or userAction.lower() == "create"):
         createRecord(c)
@@ -29,48 +25,55 @@ def main():
     conn.close()
 
 # define the create function
-
-
 def createRecord(c):
-    name = input("Enter the name to insert: ")
+    #get the new record data 
+    name = input("\nEnter the name to insert: ")
     email = input("Enter the email to insert: ")
+
     # insert the values into the database
     values = (name, email)
     sql = "INSERT INTO CONTACT (NAME,EMAIL) VALUES (?,?)"
     c.execute(sql, values)
 
 # define the update record function
-
-
 def updateRecord(c):
+    #get and print all the records 
     c.execute("SELECT * FROM CONTACT")
     rows = c.fetchall()
+
+    #print empty line
+    print(" ")
 
     for r in rows:
         print(r)
 
-    recordID = input("Enter the ID of the Record to update: ")
+    #get the information for updating the selected record
+    recordID = input("\nEnter the ID of the Record to update: ")
     name = input("Enter a new name for the record: ")
     email = input("Enter a new email for the record: ")
 
+    #update the record with the new information
     sqlite_update_query = "UPDATE CONTACT SET NAME = ?, EMAIL = ? WHERE CONTACT_ID = ?"
     data = (name, email, recordID)
     c.execute(sqlite_update_query, data)
 
 # define the delete record function
-
-
 def deleteRecord(c):
+    #get and print all the rows
     c.execute("SELECT * FROM CONTACT")
     rows = c.fetchall()
 
+    #print empty line
+    print(" ")
+        
     for r in rows:
         print(r)
 
-    recordID = input("Enter the ID of the Record to delete: ")
+    #get the recordID and delete its record
+    recordID = input("\nEnter the ID of the Record to delete: ")
 
     sqlite_update_query = "DELETE FROM CONTACT WHERE CONTACT_ID = ?"
     c.execute(sqlite_update_query, (recordID,))
 
-
+#call the main function to run the program
 main()
