a228d31c1955f7834ef48c086448279ac2db86c1
wprudencio97 <wprudencio97@gmail.com>
2020-02-23 17:37:37 -0500

    Added create and read to edit_db.py

--- create_database.py
+++
@@ -1,4 +0,0 @@
-import xlrd
-book = xlrd.open_workbook("vehicleData.xlsx")
-for sheet in book.sheets():
-    print(sheet.name)
---
+++ edit_db.py
@@ -0,0 +1,99 @@
+import sqlite3
+from tabulate import tabulate
+
+# define the main function
+def main():
+    conn = sqlite3.connect("vehicle.db")
+    cur = conn.cursor()
+
+    #get table selection 
+    tableSelection = int(input("\n1: Vehicle Table \n2: Maintenance Table \nEnter the number of the table to edit:"))
+    
+    if tableSelection == 1:
+        tableSelection = "vehicleTable"
+    elif tableSelection == 2:
+        tableSelection = "maintenanceTable"
+
+    # get the user action and call the appropriate function
+    userAction = input("\nEnter (C)reate, (R)ead, (U)pdate, or (D)elete: ")
+
+    if userAction.lower() == "c" or userAction.lower() == "create":
+        createRecord(cur, tableSelection)
+    elif userAction.lower() == "r" or userAction.lower() == "read":
+        printTable(cur, tableSelection)
+   
+    #Save (commit) the changes
+    conn.commit()
+    # Close the connection.
+    conn.close()
+
+def createRecord(cur, tableName):
+    #get the rowHeaders using getTableHeaders
+    rowHeaders = getTableHeaders(cur, tableName)
+    #get the values to insert data to
+    longString = getString(rowHeaders)
+    #get the amount of values to be inserted
+    valueCount = getCount(rowHeaders)
+
+    #get the values to input
+    valuesList = getValues(rowHeaders)
+
+    # insert the values into the database
+    values = valuesList
+    sql = f"INSERT INTO {tableName} ({longString}) VALUES ({valueCount})"
+    cur.execute(sql, values)
+
+def getTableHeaders(cur, tableName):
+    cur.execute(f"SELECT * FROM {tableName}")
+    colnames = cur.description
+    
+    #read the row headers and store them in a list
+    rowHeaders =[]
+    for row in colnames:
+        rowHeaders.append(row[0])
+    #return the rowHeaders list
+    return rowHeaders
+
+def getValues(list):
+    valueList = []
+
+    for i in list:
+        i = input(f"Enter the {i}: ")
+        valueList.append(i)
+
+    return valueList 
+
+def printTable(cur, tableName):
+    cur.execute(f"SELECT * FROM {tableName}")
+    rows = cur.fetchall()
+    colnames = cur.description
+
+    #read the row headers and store them in a list
+    rowHeaders =[]
+    for row in colnames:
+        rowHeaders.append(row[0])
+    
+    #print the table
+    print("")#empty line
+    print(tabulate(rows, headers=rowHeaders))
+
+def getString(list):
+    stringData = ""
+    for i in list:
+        stringData = stringData + "," + str(i)
+
+    #remove the first character(,) from stringData    
+    stringData = stringData[1:]
+
+    return stringData
+
+def getCount(list):
+    stringCount = "?"
+
+    for i in range(1,len(list)):
+        stringCount = stringCount + ",?"
+
+    return stringCount
+
+
+main() #run the program
